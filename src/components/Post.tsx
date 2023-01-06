46385934df4f956b8747f5b0f8387fc028305ac3
Beantly <jbockal1501@gmail.com>
2023-01-06 16:03:14 +0100

    I think UserPage skeleton is finished??
    Needs styling, also LOCAL STORAGE FOR LIKES???????? idk if i want that..

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -14,6 +14,7 @@ interface PostProps {
 function Post(props: PostProps) {
 
   const [likes, setLikes] = useState<number>(0);
+  const [username, setUsername] = useState<string>("");
   const [interacted, setInteracted] = useState<number>(0);
 
   const navigate = useNavigate();
@@ -24,6 +25,14 @@ function Post(props: PostProps) {
       setInteracted(props.interacted);
     }
   }, [])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${props.userId}`)
+      .then(res => res.json())
+      .then(data => setUsername(data.username));
+  })
+
+
 
   function handleLikes(num: number) {
     if (num === 1 && interacted === 0) {
@@ -55,11 +64,19 @@ function Post(props: PostProps) {
     }
   }
 
+  function handleUser() {
+    navigate(`/users/${props.userId}`);
+  }
+
   return (
     <div className='border-white border-2 p-5 transform hover:custom-shadow relative flex flex-col gap-5 bg-reddit-grey'>
+      <>
+        <img src="/images/profileIcon.jpg" alt="profileIcon" className='w-[2rem] absolute right-5 bottom-5 cursor-pointer' onClick={handleUser} />
+        <p className='absolute right-16 bottom-6 text-white hidden sm:inline cursor-pointer' onClick={handleUser}>{username}</p>
+      </>
       <div className='cursor-pointer' onClick={handleNavigate}>
         <p className='text-white pb-5 font-bold'>{props.title}</p>
-        <p className='text-white pb-'>{props.body}</p>
+        <p className='text-white'>{props.body}</p>
       </div>
       <div>
         <div className='text-white gap-2 items-center cursor-pointer inline-flex'>
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Post from '../components/Post';
 
 function UserPage() {
+
+  function generateLikes() {      //generates a random amount of likes, between -100 and 100, just because if not, all of them would be 0
+    return Math.floor(Math.random() * (101 + 100) - 100)
+  }
 
   interface UserInterface {
     id: number;
@@ -10,19 +15,50 @@ function UserPage() {
     email: string;
   }
 
-  const [users, setUsers] = useState<UserInterface[]>();
+  interface PostInterface {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+    clickable?: boolean;
+  }
+
+  const [user, setUser] = useState<UserInterface>();
+  const [posts, setPosts] = useState<PostInterface[]>();
 
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then(res => res.json())
-      .then(data => setUsers(data));
+      .then(data => setUser(data));
   }, []);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then(res => res.json())
+      .then(data => setPosts(data));
+  }, []);
+
+
+  {/* <Post key={post.id} postId={post.id} userId={post.userId} title={post.title} body={post.body} likes={amountOfLikes} /> */ }
+
 
   return (
     <div>
-      {users?.map((user) => <p>id: {user.id}, name: {user.name}, username: {user.username}</p>)}
+      {/* <p>id: {user?.id}, name: {user?.name}, username: {user?.username}</p> */}
+      {posts?.map((post) => {
+        if (id) {
+          if (post.userId === parseInt(id, 10)) {
+
+            var amountOfLikes = generateLikes();
+
+            return (
+              <Post key={post.id} postId={post.id} userId={post.userId} title={post.title} body={post.body} likes={amountOfLikes} />
+            )
+          }
+        }
+      })}
     </div>
   )
 }
